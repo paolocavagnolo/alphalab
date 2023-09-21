2adc0cda52ea9a2ef00e50ed302538b39edbc76c
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2023-09-21 12:52:57 +0200

    Add files via upload

---
+++ cattura_volto/_showImage.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from tkinter.ttk import *
+from PIL import Image, ImageTk
+import sys
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("echo")     # naming it "echo"
+args = parser.parse_args()  # returns data from the options specified (echo)
+
+image_path = args.echo
+
+def close_window ():
+    global root
+    root.destroy()
+
+pilImage = Image.open(image_path)
+
+root = Tk()
+
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+
+root.attributes('-fullscreen', True)
+root.geometry("%dx%d+0+0" % (w, h))
+root.focus_set()
+root.bind("<Escape>", lambda e: root.destroy())
+root.after(5000,lambda:root.withdraw())
+root.after(5100,lambda:root.destroy())
+
+canvas = Canvas(root,width=w,height=h,highlightthickness=0)
+canvas.pack()
+canvas.configure(background='black')
+
+imgWidth, imgHeight = pilImage.size
+
+if imgWidth > w or imgHeight > h:
+    ratio = min(w/imgWidth, h/imgHeight)
+    imgWidth = int(imgWidth*ratio)
+    imgHeight = int(imgHeight*ratio)
+    pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+
+image = ImageTk.PhotoImage(pilImage)
+imagesprite = canvas.create_image(w/2,h/2,image=image)
+
+root.mainloop()
+
+
